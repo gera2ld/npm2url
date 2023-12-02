2aa2514fa1c573958343a8aee7b5f2bfae286b39
Gerald <code@gera2ld.space>
2023-12-03 00:55:51 +0800

    fix: abort slow requests

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,16 @@ const defaultProviders: Record<string, (path: string) => string> = {
   unpkg: (path: string) => `https://unpkg.com/${path}`,
 };
 
+async function checkUrl(url: string, signal: AbortSignal) {
+  const res = await fetch(url, {
+    signal,
+  });
+  if (!res.ok) {
+    throw res;
+  }
+  await res.text();
+}
+
 export class UrlBuilder {
   providers = { ...defaultProviders };
 
@@ -14,24 +24,27 @@ export class UrlBuilder {
    * Get the fastest provider name.
    * If none of the providers returns a valid response within `timeout`, an error will be thrown.
    */
-  getFastestProvider(timeout = 5000, path = testPath) {
-    return new Promise<string>((resolve, reject) => {
-      Promise.all(
-        Object.entries(this.providers).map(async ([name, factory]) => {
-          try {
-            const res = await fetch(factory(path));
-            if (!res.ok) {
-              throw res;
+  async getFastestProvider(timeout = 5000, path = testPath) {
+    const controller = new AbortController();
+    let timer = 0;
+    try {
+      return await new Promise<string>((resolve, reject) => {
+        Promise.all(
+          Object.entries(this.providers).map(async ([name, factory]) => {
+            try {
+              await checkUrl(factory(path), controller.signal);
+              resolve(name);
+            } catch {
+              // ignore
             }
-            await res.text();
-            resolve(name);
-          } catch {
-            // ignore
-          }
-        })
-      ).then(() => reject(new Error("All providers failed")));
-      setTimeout(reject, timeout, new Error("Timed out"));
-    });
+          })
+        ).then(() => reject(new Error("All providers failed")));
+        timer = setTimeout(reject, timeout, new Error("Timed out"));
+      });
+    } finally {
+      controller.abort();
+      clearTimeout(timer);
+    }
   }
 
   /**
